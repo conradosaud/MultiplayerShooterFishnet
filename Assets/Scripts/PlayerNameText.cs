688f5c8f8bc7815666d49712c9a821d27749eb99
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2024-04-12 21:59:13 -0300

    playername overhead

---
+++ Assets/Scripts/PlayerNameText.cs
@@ -0,0 +1,26 @@
+using FishNet.Object;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerNameText : NetworkBehaviour
+{
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        if( IsOwner)
+        {
+            transform.GetComponent<TextMeshProUGUI>().enabled = false;
+        }
+
+    }
+
+    private void Update()
+    {
+        //transform.LookAt(Camera.main.transform);
+        transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+    }
+}
